f96e9614f42bf8958a885ebaf2b20d1c5561bbc4
Gaston Amigo <gastonamigo@gmail.com>
2022-11-05 13:20:20 -0300

    modificaciones en checkout y readme

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,67 +1,73 @@
 import './Checkout.css'
 import { useState, useContext } from "react"
 import { CartContext } from "../../context/CartContext"
-import { collection, getDocs, query, where, documentId, writeBatch, addDoc } from 'firebase/firestore'
+import { collection, getDocs, query, where, documentId, writeBatch, addDoc, Timestamp } from 'firebase/firestore'
 import { db } from '../../services/firebase/index'
 import { useNavigate } from "react-router-dom"
-import  ClientForm  from '../Form/Form'
+import ClientForm from '../Form/Form'
 import Swal from 'sweetalert2'
 
 const Checkout = () => {
     const [loading, setLoading] = useState(false)
 
-    const [personalData, setPersonalData] = useState(false)
-    
-            const [datosCompra, setDatosCompra] = useState({}) 
+    const [clientData, setClientData] = useState(false)
 
-    const information = (name, lastname, address, phone, email) =>{
-            setDatosCompra({name, lastname, address, phone, email})
-            setPersonalData(true)
-        }
-    
+    const [buyerData, setBuyerData] = useState({})
 
-    const { cart, total, clearCart} = useContext(CartContext)
+    const { cart, total, clearCart } = useContext(CartContext)
 
     const navigate = useNavigate()
-    
+
+    const information = (name, lastname, address, phone, email) => {
+        setBuyerData({ name, lastname, address, phone, email })
+        setClientData(true)
+    }
+
+
     const createOrder = async () => {
         setLoading(true)
 
         try {
             const objOrder = {
-                buyer: datosCompra,
+                buyer: buyerData,
                 items: cart,
-                total: total
+                total: total,
+                date: Timestamp.fromDate(new Date())
             }
-            console.log (objOrder)
+
+            console.log(objOrder)
+
             const batch = writeBatch(db)
 
             const outOfStock = []
 
             const ids = cart.map(prod => prod.id)
-            
-    
+
+
             const productsRef = collection(db, 'Products')
-    
+
             const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
 
             const { docs } = productsAddedFromFirestore
 
             docs.forEach(doc => {
+
                 const dataDoc = doc.data()
+
                 const stockDb = dataDoc.stock
 
                 const productAddedToCart = cart.find(prod => prod.id === doc.id)
+
                 const prodQuantity = productAddedToCart?.quantity
 
-                if(stockDb >= prodQuantity) {
+                if (stockDb >= prodQuantity) {
                     batch.update(doc.ref, { stock: stockDb - prodQuantity })
                 } else {
-                    outOfStock.push({ id: doc.id, ...dataDoc})
+                    outOfStock.push({ id: doc.id, ...dataDoc })
                 }
             })
 
-            if(outOfStock.length === 0) {
+            if (outOfStock.length === 0) {
                 await batch.commit()
 
                 const orderRef = collection(db, 'orders')
@@ -74,42 +80,43 @@ const Checkout = () => {
                     navigate('/')
                 }, 2000)
                 console.log(`El id de su orden es: ${orderAdded.id}`)
-                
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'La orden de compra se realizo con exito',
-                        text: `Codigo de operacion: ${orderAdded.id}`,
-                                                
-                    })
-               
-                
-            } 
+
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'La orden de compra se realizo con exito',
+                    html: `Codigo de operacion: ${orderAdded.id}`
+                })
+
+
+            }
 
         } catch (error) {
             console.log(error)
         } finally {
             setLoading(false)
         }
-        
+
     }
 
 
-     
-    if(loading) {
+
+    if (loading) {
         return <h1>Se esta procesando su pedido...</h1>
     }
-    
 
 
 
-    return (    
+
+    return (
         <div className='checkout'>
             <h1 className='titulo'>Completa los siguientes datos.</h1>
-            <ClientForm information={information}/>
-            { personalData 
-           
-            ?<button className='ButtonConfirm' onClick={createOrder}>Generar Pedido</button> 
-            : ""}
+            <ClientForm information={information} />
+            {clientData
+
+                ? <button className='ButtonConfirm' onClick={createOrder}>Generar Pedido</button>
+
+                : ""}
         </div>
     )
 }
